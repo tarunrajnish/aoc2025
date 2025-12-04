d6d086c026e65ac903f1d734ff312e1523d7c1f6
trajnish <trajnish@starbucks.com>
2025-12-03 16:26:50 -0800

    day 2 and 3

---
+++ dec2/Program.cs
@@ -0,0 +1,151 @@
+﻿class Program{
+    static void Main(string[] args)
+    {
+        string inputPathSml = "input_2_sml.txt";
+        string inputPath = "input_2.txt";
+
+        string[] lines = File.ReadAllLines(inputPath);
+
+        // PrintLines(lines);
+        
+        string[] ranges = lines[0].Split(',');
+        Console.WriteLine($"length of ranges: {ranges.Length}");
+        Console.WriteLine($"ranges: {string.Join(", ", ranges)}");
+
+        // long invalidSum = GetInvalidSum(ranges);
+        // Console.WriteLine($"invalidSum: {invalidSum}");
+
+        long invalidSum2 = GetInvalidSum2(ranges);
+        Console.WriteLine($"invalidSum2: {invalidSum2}");
+    }
+
+    static long GetInvalidSum2(string[] ranges)
+    {
+        long invalidSum = 0;
+        List<long> invalidList = new List<long>();
+
+        foreach(string range in ranges)
+        {
+            long lowerBound = long.Parse(range.Split('-')[0]);
+            long upperBound = long.Parse(range.Split('-')[1]);
+
+            // Console.WriteLine($"lower, upperBound: {lowerBound}, {upperBound}");
+            
+
+            for (long i=lowerBound; i <= upperBound; i++)
+            {
+                int l = 0;
+                int r = 1;
+                
+                string id = i.ToString();
+                Console.WriteLine($"id: {id}");
+                int idLength = id.Length;
+                int halfLength = idLength / 2;
+                
+
+                while (r <= halfLength)
+                {
+                    string group = id.Substring(l,r);
+                    // Console.WriteLine($"group: {group}");
+                    int groupLength = group.Length;
+                    // Console.WriteLine($"groupLength: {groupLength}");
+
+                    int parts = (int)Math.Ceiling((double)idLength / groupLength);
+                    // Console.WriteLine($"parts: {parts}");
+
+                    int index = 0;
+                    List<string> compareList = new List<string>();
+
+                    for (int p=0; p<parts; p++)
+                    {
+                        try{
+                            compareList.Add(id.Substring(index, groupLength));
+                        }
+                        catch
+                        {
+                            compareList.Add("0");
+                        }
+                        index += groupLength;
+                    }
+
+                    if (compareList.Count < 2)
+                    {
+                        break;
+                    }
+
+                    bool allEqual = compareList.All(s => s==group);
+                    if (allEqual)
+                    {
+                        Console.WriteLine($"ALL EQUAL in id for group: {id}, {group}");
+                        // foreach(string comp in compareList)
+                        // {
+                        //     Console.WriteLine(comp);
+                        // }
+                        if (!invalidList.Contains(long.Parse(id)))
+                        {
+                            invalidList.Add(long.Parse(id));
+                        }
+                    }
+                    r ++;
+                }
+            }
+        }
+
+        foreach(long id in invalidList)
+        {
+            Console.WriteLine(id);
+            invalidSum += id;
+        }
+        return invalidSum;
+    }
+
+    static long GetInvalidSum(string[] ranges)
+    {
+        long invalidSum = 0;
+
+        foreach(string range in ranges)
+        {
+            long lowerBound = long.Parse(range.Split('-')[0]);
+            long upperBound = long.Parse(range.Split('-')[1]);
+
+            // Console.WriteLine($"lower, upperBound: {lowerBound}, {upperBound}");
+
+            for (long i=lowerBound; i <= upperBound; i++)
+            {
+                // Console.WriteLine(i);
+                int idLength = i.ToString().Length;
+                // Console.WriteLine($"idLength: {idLength}");
+
+                if (idLength%2 != 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    int halfLength = idLength / 2;
+                    string firstHalf = i.ToString().Substring(0,halfLength);
+                    string secondHalf = i.ToString().Substring(halfLength);
+                    // Console.WriteLine($"halfLength, firstHalf, secondHalf: {halfLength}, {firstHalf}, {secondHalf}");
+
+                    if (long.Parse(firstHalf) == long.Parse(secondHalf))
+                    {
+                        Console.WriteLine($"Invalid ID: {i}");
+                        invalidSum += i;
+                    }
+                }
+            }
+        }
+
+        return invalidSum;
+    }
+
+    static void PrintLines(string[] lines)
+    {
+        Console.WriteLine($"length of lines: {lines.Length}");
+
+        foreach(var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
---
+++ dec3/Program.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputPathSml = "input_3_sml.txt";
+        string inputPath = "input_3.txt";
+
+        string[] lines = File.ReadAllLines(inputPathSml);
+
+        // PrintLines(lines);
+
+        // long sumMaxJoltage = getMaxJoltage(lines);
+        // Console.WriteLine($"sumMaxJoltage: {sumMaxJoltage}");
+
+        long sumMaxJoltage2 = getMaxJoltage2(lines);
+        Console.WriteLine($"sumMaxJoltage2: {sumMaxJoltage2}");
+    }
+
+    static long getMaxJoltage2(string[] lines)
+    {
+        int maxStackLength = 12;
+        long sumMaxJoltage = 0;
+
+        foreach(string line in lines)
+        {   
+            Stack<char> stack = new Stack<char>();
+            Console.WriteLine($"line: {line}");
+
+            foreach(char c in line)
+            {
+                int count = stack.Count;
+
+                if (count == 0)
+                {
+                    stack.Push(c);
+                    Console.WriteLine("FIRST PUSH!");
+                    
+                    foreach (char item in stack)
+                    {
+                        Console.Write(item + " ");
+                    }
+
+                } 
+                else
+                {
+                    char peekValue = stack.Peek();
+                    Console.WriteLine($"peakValue: {peekValue}");
+
+                    if (c > peekValue)
+                    {
+                        stack.Pop();
+                        stack.Push(c);
+                    }
+                    else
+                    {
+                        if (count < maxStackLength)
+                        {
+                            stack.Push(c);
+                        } else
+                        {
+                            if (c > peekValue)
+                            {
+                                stack.Pop();
+                                stack.Push(c);
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Printing stack");
+                    foreach (char item in stack)
+                    {
+                        Console.Write(item + " ");
+                    }
+                }
+            }
+        }
+
+        return sumMaxJoltage;
+    }
+
+    static long getMaxJoltage(string[] lines)
+    {
+        long sumMaxJoltage = 0;
+
+        foreach(string line in lines)
+        {
+            int maxJoltage = 0;
+            Console.WriteLine($"line: {line}");
+            int lMax = 0;
+
+            for (int i=0; i<line.Length-1; i++)
+            {
+                int rMax = 0;
+                char c = line[i];
+                int joltage = int.Parse(c.ToString());
+                // Console.WriteLine($"joltage: {joltage}");
+
+                lMax = Math.Max(lMax, joltage);
+
+                for (int j=i+1; j<line.Length; j++)
+                {
+                    char innerC = line[j];
+                    int innerJoltage = int.Parse(innerC.ToString());
+                    rMax = Math.Max(rMax, innerJoltage);
+                }
+                // Console.WriteLine($"lmax, rmax: {lMax}, {rMax}");
+                string maxString = lMax.ToString() + rMax.ToString();
+                // Console.WriteLine($"maxString: {maxString}");
+                maxJoltage = Math.Max(maxJoltage, int.Parse(maxString));
+            }
+            Console.WriteLine($"maxJoltage: {maxJoltage}");
+            sumMaxJoltage += maxJoltage;
+        }
+
+        return sumMaxJoltage;
+    }
+
+    static void PrintLines(string[] lines)
+    {
+        Console.WriteLine($"length of lines: {lines.Length}");
+
+        foreach(var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
